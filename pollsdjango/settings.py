2125e33f4e4d73134b9595a5c082e2884017bf4f
Denis <d.b.shadrin@urfu.ru>
2023-04-05 19:04:24 +0500

    ALLOWED_HOSTS

--- pollsdjango/settings.py
+++ pollsdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+1fbxl202p@@gjbh&(tp5%6%=motfa0hqm$_8zca1l1ztanq*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dbshadrin.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
